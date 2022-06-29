e86f5d956d23817c0526376a95d63fd73efcdfe3
Aurel-GitHub <aurelien.landouer@hotmail.fr>
2022-06-29 18:18:18 +0200

    update corsOptions

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,29 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
   app.use(
     helmet({
-      contentSecurityPolicy: false,
+      contentSecurityPolicy:
+        process.env.NODE_ENV === 'production' ? undefined : false,
     }),
   );
+  const corsOptions = {
+    origin: (
+      origin: string | undefined,
+      callback: (err: Error | null, check?: boolean) => Error | void,
+    ) => {
+      if (process.env.NODE_ENV === 'development') {
+        return callback(null, true);
+      }
+      // if (whitelistedUrls.indexOf(origin as string) !== -1) {
+      //   return callback(null, true);
+      // }
+      return callback(new Error('Not allowed by CORS'));
+    },
+    credentials: true,
+    exposedHeaders: ['x-authorization'],
+  };
+  app.enableCors(corsOptions);
 
   await app.listen(process.env.PORT || 5000);
 }
